ac2e8d0e46e2cc9495d700b92f8c9880ea83a9cb
Pham Duy Thanh <pham.duy.thanh@framgia.com>
2019-11-05 08:39:01 +0700

    Init project

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use \Laravel\Socialite\Facades\Socialite;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+            $user = Socialite::driver('google')->user();
+            $findUser = User::where('google_id', $user->getId())->first();
+            if ($findUser) {
+                \Auth::login($findUser);
+
+                return redirect('/home');
+            } else {
+                $newUser = User::create([
+                    'name' => $user->getName(),
+                    'email' => $user->getEmail(),
+                    'google_id' => $user->getId(),
+                    'password' => Hash::make('password'),
+                ]);
+
+                \Auth::login($newUser);
+
+                return redirect()->back();
+            }
+
+        } catch (\Exception $e) {
+            return redirect('auth/google');
+        }
+    }
+}
